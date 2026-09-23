471457e3b1702bf4881725c5f30430a84c7f04ea
AungSoe Moe <61192036+whispermmepub@users.noreply.github.com>
2026-09-23 22:29:59 +0700

    Add Mobile Use Android project files

---
+++ app/build.gradle.kts
@@ -0,0 +1,4 @@
+plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
+android { namespace="com.whisper.mobileuse"; compileSdk=35
+ defaultConfig { applicationId="com.whisper.mobileuse"; minSdk=26; targetSdk=35; versionCode=1; versionName="0.1.0" }
+}
